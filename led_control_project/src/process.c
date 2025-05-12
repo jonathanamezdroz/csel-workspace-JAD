fda794b4098f268a744489f631ae387b7377c55c
adrien balleyguier <adriendino.balleyguier@master.hes-so.ch>
2025-05-12 21:02:09 +0200

    feat: adding select as is. It DOES NOT WORK for reasons that seems to be as:
    calling read(...) on switch fd does not consume the readable flag thus having the select triggers immediately

--- led_control_project/src/process.c
+++ led_control_project/src/process.c
@@ -1,5 +1,9 @@
 #include "process.h"
 
+static int DUTY_CYCLE_ON = 50;
+
+//--- silly approach starts here
+
 void silly_process(long period){
     long duty   = 2;    
     period *= 1000000;  // in ns
@@ -35,10 +39,120 @@ void silly_process(long period){
     return;
 }
 
+//--- silly approach ends here
+//--- select approach starts here
+
+static int get_max_fd(int nb_fd, ...){
+    int ret = -1;   //expecting any fd >= 1
+    int i, tmp;
+    va_list fds;
+    va_start(fds, nb_fd);
+    for(i = 0;i<nb_fd;i++){
+        tmp = va_arg(fds, int);
+        if(ret < tmp){
+            ret = tmp;
+        }else{}
+    }
+    va_end(fds);
+    return ret;
+}
+
+static void prepare_select_loop(fd_set *select_fd, int nb_fd, ...){
+    int i, tmp;
+    FD_ZERO(select_fd);
+    va_list fds;
+    va_start(fds, nb_fd);
+    for(i = 0;i<nb_fd;i++){
+        tmp = va_arg(fds, int);
+        FD_SET(tmp, select_fd);
+    }
+    va_end(fds);
+}
+
+void select_process(long period){
+    long default_period = period;
+    long current_period = default_period;
+    long delta_period = default_period * 10 / 100;  //10% of default period
+    int select_status;
+    int timer_on_fd, timer_off_fd, led_fd, k1_fd, k2_fd, k3_fd, largest_fd;
+    char buf;
+    long tmp_long;
+    fd_set fd_in;
+    struct timeval timeout = {.tv_sec = 5, .tv_usec = 0};
+    FD_ZERO(&fd_in);
+    // create fd and define associated events
+    //switch K1
+    k1_fd = open_switch(K1, GPIO_K1);
+    lseek(k1_fd, 0, SEEK_SET);
+    read(k1_fd, &buf, 1);
+    //switch K2
+    k2_fd = open_switch(K2, GPIO_K2);
+    lseek(k2_fd, 0, SEEK_SET);
+    read(k2_fd, &buf, 1);
+    //switch K3
+    k3_fd = open_switch(K3, GPIO_K3);
+    lseek(k3_fd, 0, SEEK_SET);
+    read(k3_fd, &buf, 1);
+    //timer to activate the led
+    timer_on_fd = start_timer(current_period, 0);
+    //timer to deactivate the led
+    timer_off_fd = start_timer(current_period, DUTY_CYCLE_ON);
+    //led fd
+    led_fd = open_led();
+    largest_fd = get_max_fd(5, timer_on_fd, timer_off_fd, k1_fd, k2_fd, k3_fd);
+    while(1){
+        prepare_select_loop(&fd_in, 5, timer_on_fd, timer_off_fd, k1_fd, k2_fd, k3_fd);
+        timeout.tv_sec = 5;
+        timeout.tv_usec = 0;
+        select_status = select(largest_fd+1, &fd_in, NULL, NULL, &timeout);
+        if(select_status < 0){
+            syslog(LOG_NOTICE, "error occured with select\n");
+            break;
+        }else if(0 == select_status){
+            //timeout
+        }else{
+            if(FD_ISSET(timer_on_fd, &fd_in)){
+                read(timer_on_fd, &tmp_long, sizeof(tmp_long));
+                write(led_fd, "1", 1);
+            }else{}
+            if(FD_ISSET(timer_off_fd, &fd_in)){
+                read(timer_off_fd, &tmp_long, sizeof(tmp_long));
+                write(led_fd, "0", 1);
+            }else{}
+            if(FD_ISSET(k1_fd, &fd_in)){
+                lseek(k1_fd, 0, SEEK_SET);
+                read(k1_fd, &buf, 1);
+                current_period -= delta_period;
+                update_timer(timer_on_fd, current_period, 0);
+                update_timer(timer_off_fd, current_period, DUTY_CYCLE_ON);
+            }else{}
+            if(FD_ISSET(k2_fd, &fd_in)){
+                lseek(k2_fd, 0, SEEK_SET);
+                read(k2_fd, &buf, 1);
+                lseek(k2_fd, 0, SEEK_SET);
+                current_period = default_period;
+                update_timer(timer_on_fd, current_period, 0);
+                update_timer(timer_off_fd, current_period, DUTY_CYCLE_ON);
+            }else{}
+            if(FD_ISSET(k3_fd, &fd_in)){
+                lseek(k3_fd, 0, SEEK_SET);
+                read(k3_fd, &buf, 1);
+                current_period += delta_period;
+                update_timer(timer_on_fd, current_period, 0);
+                update_timer(timer_off_fd, current_period, DUTY_CYCLE_ON);
+            }else{}
+        }
+    }
+    //unregister fd and close them
+    return;
+}
+
+//--- select approach ends here
+//--- epoll approach starts here
+
 static int MAX_EVENT_FOR_SINGLE_LOOP = 8;
 static int EPOLL_WAIT_TIMEOUT = -1; // wait forever until an event occurs
 #define READ_BUFFER_SIZE 128
-static int DUTY_CYCLE_ON = 50;
 
 static void add_to_epoll(int epoll_fd, int fd, uint32_t flags){
     long tmp_long;
@@ -131,3 +245,5 @@ void epoll_process(long period){
     // good practice would require to unregister file descriptors and close them
     return;
 }
+
+//--- epoll approache ends here
--- led_control_project/src/process.h
+++ led_control_project/src/process.h
@@ -4,7 +4,9 @@
 #include <unistd.h>
 #include <errno.h>
 #include <syslog.h>
+#include <sys/select.h>
 #include <sys/epoll.h>
+#include <stdarg.h>
 
 #include "led_control.h"
 #include "switch_control.h"
@@ -12,5 +14,6 @@
 
 void silly_process(long period);
 void epoll_process(long period);
+void select_process(long period);
 
 #endif // PROCESS_H
